ac11352656e4f4da650c4d7a217d50ae4300f12e
Kiyo510 <miraishida00510@gmail.com>
2023-12-30 00:07:06 +0900

    :lipstick: set AstroVim and statusline theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,31 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  {
+    {
+      "nvim-lualine/lualine.nvim",
+      event = "VeryLazy",
+      opts = {
+        options = {
+          theme = "gruvbox_dark",
+        },
+        sections = {
+          lualine_c = {
+            {
+              "filename",
+              path = 1,
+            },
+          },
+        },
+      },
+    },
+    -- {
+    --   "rebelot/heirline.nvim",
+    --   optional = true,
+    --   opts = function(_, opts) opts.statusline = nil end,
+    -- },
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
